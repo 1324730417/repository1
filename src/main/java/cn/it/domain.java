1e171faaf07ba0c61dfbb923e81a0b9519a06ce7
wh <1324730417@qq.com>
2019-10-17 21:59:38 +0800

    test

--- src/main/java/cn/it/domain.java
+++ src/main/java/cn/it/domain.java
@@ -3,4 +3,12 @@ package cn.it;
 public class domain {
     private Integer id;
     private String cname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
